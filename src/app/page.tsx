655838862d4e664ee09fa1f4b3e4c224ba73b6f4
Abiola Oyewale <abiol4001@gmail.com>
2023-10-22 08:11:12 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className='text-3xl text-red-600'>Welcome</p>
+    </main>
+  )
+}
